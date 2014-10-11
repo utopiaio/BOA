c30c8c3a0124750061e726a5f73d709cfc6a26f8
Moe Szyslak <moe.duffdude@gmail.com>
2014-10-11 14:58:55 +0300

    keep the lines nice and neat
    - yet more "documentation"

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,6 +3,8 @@ var app = angular.module('app', ['ngRoute', 'ngAnimate', 'ngTouch']);
 
 
 app.controller('appCtrl', ['$rootScope', '$http', '$q', '$location', function ($rootScope, $http, $q, $location) {
+  $rootScope.io = null;
+
   $rootScope.$on('$routeChangeError', function (event, current, previous, rejection) {
     console.error(rejection);
   });
@@ -13,16 +15,18 @@ app.controller('appCtrl', ['$rootScope', '$http', '$q', '$location', function ($
   $rootScope.$on('$routeChangeSuccess', function (event, target) {
   });
 
-
-
-  $rootScope.io = null;
   $rootScope.connect = function () {
+    // we will only establish connection once
+    // after that socket.io engine will go to the end of earth to reconnect
+    // we just sit back and enjoy the ride
     if ($rootScope.io === null) {
       $rootScope.io = io.connect();
 
+      /* i think this is a bit too much info
       $rootScope.io.on('connect', function () {
         iPNotify({text: 'hurray, socket connection established', type: 'info'});
       });
+      */
 
       $rootScope.io.on('message', function (data) {
         if (data.notify !== undefined) {
@@ -30,58 +34,68 @@ app.controller('appCtrl', ['$rootScope', '$http', '$q', '$location', function ($
         }
 
         switch(data.code) {
+          // someone has created a new branch
           case 'NEW_BRANCH':
             $rootScope.$broadcast('NEW_BRANCH', data.newBranch);
           break;
 
+          // someone has updated a branch info
           case 'UPDATED_BRANCH':
             $rootScope.$broadcast('UPDATED_BRANCH', data.updatedBranch);
           break;
 
+          // i wonder what this one does --- oh, yes, someone has deleted shit
           case 'DELETED_BRANCH':
             $rootScope.$broadcast('DELETED_BRANCH', data.deletedBranchId);
           break;
 
+          // MOVE-MOVE-MOVE
+          // ping result shows a branch is down --- code BLACK!
           case 'BLACK_HAWK_DOWN':
             $rootScope.$broadcast('BLACK_HAWK_DOWN', data.data);
           break;
 
+          // TODO:
+          // i think this one is redundant
+          // i "think" (which i tend to do a lot) this can be replaced by
+          // AC-DC --- TNT
           case 'I-GOT-IT':
             $('#black-hawk-down').modal('hide');
           break;
 
+          // branch as been reported
           case 'AC-DC':
             $rootScope.$broadcast('AC-DC', data.report);
           break;
         };
       });
 
+      // all errors are treated as suspicious --- i do NOT trust you son!
       $rootScope.io.on('error', function (data) {
         iPNotify({text: 'umm, what\'s up Doc?', type: 'error'});
       });
     }
   };
 
-
-
+  // TODO - besides Megan Fox
+  // like i said this seems to be redundant, will be fixed
   $rootScope.$on('I-GO-IT', function (event, branchToBeReported) {
     $rootScope.io.emit('I-GOT-IT', branchToBeReported);
   });
 
-
-
-  $rootScope.appCtrl = this;
-
-
+  // binding to couple of DOM events, menu to be exact
+  // using activex class to avoid "collision" with bootstrap
   $('.menu a, .menu button').click(function (e) {
     $('.menu a, .menu button').removeClass('activex');
     $(this).addClass('activex');
   });
 
+  $rootScope.appCtrl = this;
 }]);
 
 
 
+// just preparing, making sure we don't forget stuff
 app.config(function($sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist([
     'self',
@@ -91,6 +105,9 @@ app.config(function($sceDelegateProvider) {
 
 
 
+// we're going to intercept request and look out for anything suspicious
+// meaning: we're going to look for property 'notify' and call `iPNotify`
+// that's pretty much it --- talk about overkill :)
 app.config(function ($routeProvider, $httpProvider, $locationProvider) {
   $httpProvider.interceptors.push(function ($rootScope, $q) {
     return {
@@ -119,8 +136,6 @@ app.config(function ($routeProvider, $httpProvider, $locationProvider) {
       }
     };
   });
-
-
 
   $routeProvider.when('/', {
     templateUrl: 'templates/pingu.html',
