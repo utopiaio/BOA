8f564a190a880cea435e5bee062481d4c34b8d90
Moe Szyslak <moe.duffdude@gmail.com>
2015-08-27 06:57:43 +0300

    proper split filter
    - proper split filter
    - base url change to reflect the new changes

--- public/assets/js/split.js
+++ public/assets/js/split.js
@@ -1,7 +1,13 @@
-angular.module('ngSplit', []).filter('split', function() {
-  return function(input) {
-    if(input) {
-      return input.match(/[0-9]{1,3}/g).join('-');
-    }
-  };
-});
+;(function(angular) {
+  'use strict';
+
+  angular.module('ngSplit', []);
+
+  angular
+    .module('ngSplit')
+    .filter('split', function() {
+      return function(input) {
+        return input ? input.match(/[0-9]{1,3}/g).join('-') : input;
+      };
+    })
+})(window.angular);
